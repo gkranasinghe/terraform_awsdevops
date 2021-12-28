28596a3f9c8d439fa803a9853d2acf90ae60bcfd
gkranasinghe <gkranasinghe@gmail.com>
2021-12-28 06:17:16 +0530

    add s3 bucket

---
+++ s3-bucket.tf
@@ -0,0 +1,25 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "devops-ps-project-bucket"
+  acl    = "private"
+
+  tags = {
+    Owner       = "user"
+    Environment = "staging"
+    Name        = "bucket"
+    project     = "awsdevops-ps"
+  }
+}
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = "devops-ps-project-bucket"
+  key    = "appspec-file"
+  source = "${abspath(path.root)}/appspec.yml"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("${abspath(path.root)}/appspec.yml")
+  depends_on = [
+    aws_s3_bucket.bucket
+  ]
+}
